1d76481204fbf36b7a19d663b82c7d930fd8aef7
Michael Rencheck <rencheck.1@live.com>
2020-01-11 17:21:47 -0600

    Partially completed B.002, service done and message added

--- tsim/src/turtle_rect.cpp
+++ tsim/src/turtle_rect.cpp
@@ -18,48 +18,19 @@
 
 #include "geometry_msgs/Twist.h"
 
-// Read in yaml file and print via ros info
+int g_turtle_state = 0;
+int g_cycle_cnt = 0;
+int g_x, g_y;
 
-void callback_reset_traj()
+
+void teleport_turtle()
 {
 
-}
+  ros::NodeHandle temp_n;
 
-int main(int argc, char **argv)
-{
+  ros::ServiceClient temp_client_telabs = temp_n.serviceClient<turtlesim::TeleportAbsolute>("turtle1/teleport_absolute");
+  ros::ServiceClient temp_client_pen = temp_n.serviceClient<turtlesim::SetPen>("turtle1/set_pen");
 
-  ros::init(argc, argv, "turtle_rect");
-  ros::NodeHandle n;
-
-  ros::ServiceClient client_telabs = n.serviceClient<turtlesim::TeleportAbsolute>("turtle1/teleport_absolute");
-  ros::ServiceClient client_pen = n.serviceClient<turtlesim::SetPen>("turtle1/set_pen");
-
-  ros::ServiceServer reset = n.advertiseService("reset_traj", callback_reset_traj);
-
-  ros::Publisher pub_vel = n.advertise<geometry_msgs::Twist>("turtle1/cmd_vel", 2);
-
-
-  // Retrieve params from Parameter Server
-  int x, y, width, height, trans_vel, rot_vel, frequency;
-
-  n.getParam("x", x);
-  n.getParam("y", y);
-  n.getParam("width", width);
-  n.getParam("height", height);
-  n.getParam("trans_vel", trans_vel);
-  n.getParam("rot_vel", rot_vel);
-  n.getParam("frequency", frequency);
-
-  //Print params from the Server.
-  ROS_INFO("Got Param: %d", x);
-  ROS_INFO("Got Param: %d", y);
-  ROS_INFO("Got Param: %d", width);
-  ROS_INFO("Got Param: %d", height);
-  ROS_INFO("Got Param: %d", trans_vel);
-  ROS_INFO("Got Param: %d", rot_vel);
-  ROS_INFO("Got Param: %d", frequency);
-
-  // Wait for turtlesim services
   ros::service::waitForService("turtle1/teleport_absolute");
   ros::service::waitForService("turtle1/set_pen");
 
@@ -69,27 +40,70 @@ int main(int argc, char **argv)
 
   // Turn the pen off
   pen.request.off = 1;
-  client_pen.call(pen);
+  temp_client_pen.call(pen);
 
   // teleport to lower left corner
-  tel.request.x = x;
-  tel.request.y = y;
+  tel.request.x = g_x;
+  tel.request.y = g_y;
   tel.request.theta = 0;
-  client_telabs.call(tel);
+  temp_client_telabs.call(tel);
 
   // Turn the pen on
   pen.request.off = 0;
-  client_pen.call(pen);
+  temp_client_pen.call(pen);
 
+  g_turtle_state = 0;
+  g_cycle_cnt = 0;
+
+}
+
+bool callback_reset_traj(std_srvs::Empty::Request &req, std_srvs::Empty::Response &res)
+{
+  teleport_turtle();
+
+  return 1;
+}
+
+int main(int argc, char **argv)
+{
+
+  ros::init(argc, argv, "turtle_rect");
+
+  ros::NodeHandle g_n;
+
+  ros::Publisher pub_vel = g_n.advertise<geometry_msgs::Twist>("turtle1/cmd_vel", 2);
+
+  ros::ServiceServer srv_reset = g_n.advertiseService("reset_traj", callback_reset_traj);
+
+  // Retrieve params from Parameter Server
+  int width, height, trans_vel, rot_vel, frequency;
+
+  g_n.getParam("x", g_x);
+  g_n.getParam("y", g_y);
+  g_n.getParam("width", width);
+  g_n.getParam("height", height);
+  g_n.getParam("trans_vel", trans_vel);
+  g_n.getParam("rot_vel", rot_vel);
+  g_n.getParam("frequency", frequency);
+
+  //Print params from the Server.
+  ROS_INFO("Got Param: %d", g_x);
+  ROS_INFO("Got Param: %d", g_y);
+  ROS_INFO("Got Param: %d", width);
+  ROS_INFO("Got Param: %d", height);
+  ROS_INFO("Got Param: %d", trans_vel);
+  ROS_INFO("Got Param: %d", rot_vel);
+  ROS_INFO("Got Param: %d", frequency);
+
+  // Wait for turtlesim services and teleport turtle to the starting position
+  teleport_turtle();
 
   // Variables to hold the number of cycles to travel the desired disatnce at the specified velocities
   int hor_cycles, ver_cycles, turn_cycles;
 
   // Variable to track the turtle state
-  int turtle_state, cycle_cnt;
-
-  turtle_state = 0;
-  cycle_cnt = 0;
+  g_turtle_state = 0;
+  g_cycle_cnt = 0;
 
   hor_cycles = (float) (width/trans_vel) * frequency;
   ver_cycles = (float) (height/trans_vel) * frequency;
@@ -105,17 +119,17 @@ int main(int argc, char **argv)
   {
     // Use Feedforward control to move around the rectangle
 
-    switch(turtle_state)
+    switch(g_turtle_state)
     {
       case 0:
       // Horizontal Move
       v_command.linear.x = trans_vel;
       v_command.angular.z = 0;
 
-      if(cycle_cnt >= hor_cycles-1)
+      if(g_cycle_cnt >= hor_cycles-1)
       {
-        cycle_cnt = -1;
-        turtle_state++;
+        g_cycle_cnt = -1;
+        g_turtle_state++;
       }
 
       break;
@@ -124,10 +138,10 @@ int main(int argc, char **argv)
       v_command.linear.x = 0;
       v_command.angular.z = rot_vel;
 
-      if(cycle_cnt >= turn_cycles-1)
+      if(g_cycle_cnt >= turn_cycles-1)
       {
-        cycle_cnt = -1;
-        turtle_state++;
+        g_cycle_cnt = -1;
+        g_turtle_state++;
       }
 
       break;
@@ -136,10 +150,10 @@ int main(int argc, char **argv)
       v_command.linear.x = trans_vel;
       v_command.angular.z = 0;
 
-      if(cycle_cnt >= ver_cycles-1)
+      if(g_cycle_cnt >= ver_cycles-1)
       {
-        cycle_cnt = -1;
-        turtle_state++;
+        g_cycle_cnt = -1;
+        g_turtle_state++;
       }
 
       break;
@@ -148,10 +162,10 @@ int main(int argc, char **argv)
       v_command.linear.x = 0;
       v_command.angular.z = rot_vel;
 
-      if(cycle_cnt >= turn_cycles-1)
+      if(g_cycle_cnt >= turn_cycles-1)
       {
-        cycle_cnt = -1;
-        turtle_state = 0;
+        g_cycle_cnt = -1;
+        g_turtle_state = 0;
       }
 
       break;
@@ -159,14 +173,11 @@ int main(int argc, char **argv)
 
     pub_vel.publish(v_command);
 
-    cycle_cnt++;
+    g_cycle_cnt++;
+    ros::spinOnce();
     r.sleep();
 
   }
 
   return 0;
 }
-
-
-
-// publish velocity commands to turtle node
